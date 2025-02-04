6f0669d80dd530b28a23781fe6cb5a78a4a6ac9c
developerSIM <geonbosim318@gmail.com>
2025-02-04 21:38:30 +0900

    2025-02-04 최종

---
+++ src/main/java/miniproject/model/repository/ReplyRepository.java
@@ -0,0 +1,9 @@
+package miniproject.model.repository;
+
+import miniproject.model.entity.ReplyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReplyRepository extends JpaRepository<ReplyEntity,Integer> {
+}
