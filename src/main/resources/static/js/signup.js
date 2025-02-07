a8bc478d6e1b444b770c182171eb90c99a075c51
Hyunseo Woo <hs001023@naver.com>
2025-02-07 17:41:01 +0900

    2/7

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -5,13 +5,13 @@ const signup = () => {
   const mnameInput = document.querySelector(".mnameInput");
   const mphoneInput = document.querySelector(".mphoneInput");
 
-  const mid = midInput.value;
+  const id = midInput.value;
   const pwd = mpwdInput.value;
   const pwdCheck = mpwdInputCheck.value;
   const name = mnameInput.value;
   const phone = mphoneInput.value;
 
-  const dataObj = { mid, mpwd, mname, mphine };
+  const dataObj = { id, pwd, name, phone };
 
   const option = {
     method: "POST",
@@ -20,6 +20,16 @@ const signup = () => {
   };
 
   fetch("매핑주소(컨트롤러)", option)
-    .then((Response) => Response)
-    .then();
+    .then((Response) => Response.json())
+    .then((data) => {
+      if (data) {
+        location.href = "src/main/resources/templates/index.html";
+        alert("가입등록 완료");
+      } else {
+        alert("가입 실패");
+      }
+    })
+    .catch((error) => {
+      alert(Error + "에러");
+    });
 };
