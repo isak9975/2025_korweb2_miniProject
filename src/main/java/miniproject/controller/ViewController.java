8e945d3deaba51fea0581e86c78ace38c3bdd01d
developerSIM <geonbosim318@gmail.com>
2025-02-07 21:19:33 +0900

    25/02/07 뷰 컨트롤러 수정

--- src/main/java/miniproject/controller/ViewController.java
+++ src/main/java/miniproject/controller/ViewController.java
@@ -1,7 +1,17 @@
 package miniproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
+
+    @GetMapping("") // 메인 페이지 반환 메서드
+    public String index() {return "index.html";}
+
+    @GetMapping("/member/signup") // 회원가입 페이지 반환 메서드
+    public String signup() {return "/member/signup.html";}
+
+    @GetMapping("/member/login") // 로그인 페이지 반환 메서드
+    public String login() {return "/member/login.html";}
 }
